4e55bbb3893770516882d3337dd1d79f4b84b6ac
anmolkannaujiya <anmolkannaujiya@outlook.com>
2026-07-13 15:31:09 +0530

    created server using RestCOntroller

---
+++ demo/src/main/java/com/Anmol/demo/server/HelloServer.java
@@ -0,0 +1,14 @@
+package com.Anmol.demo.server;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloServer {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Anmol";
+    }
+}
