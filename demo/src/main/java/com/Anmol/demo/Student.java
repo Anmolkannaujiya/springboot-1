b61bc10271cd0944e344b3a365171dff54f2c0f2
anmolkannaujiya <anmolkannaujiya@outlook.com>
2026-07-13 11:05:36 +0530

    student class

---
+++ demo/src/main/java/com/Anmol/demo/Student.java
@@ -0,0 +1,32 @@
+package com.Anmol.demo;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(){
+        System.out.println("Heloo");
+    }
+
+    public Student(int age,String name){
+        this();
+        this.age = age;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
