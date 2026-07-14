c2a1d628d2176ea14eb777709d530a42782452c4
anmolkannaujiya <anmolkannaujiya@outlook.com>
2026-07-14 12:55:10 +0530

    sending and converting json into class

--- demo/src/main/java/com/Anmol/demo/StudentServer/StudentServer.java
+++ demo/src/main/java/com/Anmol/demo/StudentServer/StudentServer.java
@@ -1,20 +1,39 @@
 package com.Anmol.demo.StudentServer;
 
+import com.Anmol.demo.Student;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentServer {
 
+    private final Student student;
+
+    public StudentServer(Student student) {
+        this.student = student;
+    }
+
     //1.store the student
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-                id : 1
-                name : Anmol
-                Department : CSE
-                Age : 24
-                """;
+    public String storeStudent(@RequestBody Learner learner){
+        int id = learner.getId();
+        String name = learner.getName();
+        int age =  learner.getAge();
+        String department = learner.getDepartment();
+
+        return "id:"+id+
+                " age:"+age+
+                " name:"+name+
+                " department:"+department;
+
+
+//        return """
+//                id : 1
+//                name : Anmol
+//                Department : CSE
+//                Age : 24
+//                """;
     }
 
     //2. Read the student
